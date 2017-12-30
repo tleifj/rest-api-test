aa315d9cfe1be8ace1b7981ad0ac7c43915a5bdf
Travis Johnson <travisjohnson@Traviss-MacBook-Pro.local>
2017-12-29 17:50:40 -0800

    Adds nodemon and morgan for better developement

--- app.js
+++ app.js
@@ -2,13 +2,19 @@
 const express = require('express');
 const app = express();
 
-const productRoutes = require('./api/routes/products');
+// Logging package for Node.js
+const morgan = require('morgan');
 
+// Include route-specifc code
+const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// Run everything through Morgan middleware
+app.use(morgan('dev'));
 
+// Routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
-
+// Export for use in server.js
 module.exports = app;
